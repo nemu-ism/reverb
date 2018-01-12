e09405d4a2746005e44d38541f45cbe68a9a6fc9
nemu-ism <vanilla_chip_s@yahoo.co.jp>
2018-01-13 06:11:32 +0900

    input修正

--- reverb.py
+++ reverb.py
@@ -1,19 +1,20 @@
 import tweepy
-from Tools.scripts.treesync import raw_input
-
 
 Consumer_key = 'JQlDOEzxGeq2eLEgErFCLzy2z'
 Consumer_secret = 'rnBWO8k0dZMaO4RjGpTudCTNWDRqIDnTm05at1sbKL6f1Fbqbi'
 auth = tweepy.OAuthHandler(Consumer_key, Consumer_secret)
 
-## get url to get user's access_token and access_token_secrets
-#rediret_url = auth.get_authorization_url()
-#print("Get your Verification code from: {:}".format(rediret_url))
-#
-#verifier = raw_input('Type the verification code: ').strip()
-#auth.get_access_token(verifier)
-#Access_token = auth.access_token
-#Access_token_secret = auth.access_token_secret
+# # get url to get user's access_token and access_token_secrets
+# rediret_url = auth.get_authorization_url()
+# print("Get your Verification code from: {:}".format(rediret_url))
+
+# verifier = input('Type the verification code: ').strip()
+# try:
+#     auth.get_access_token(verifier)
+# except:
+#     print("Failed")
+# Access_token = auth.access_token
+# Access_token_secret = auth.access_token_secret
 
 Access_token = '3148157702-Mm2cHiHoqf2jUwzy8qIYeDzJZFL9jSSGGFmphN1'
 Access_token_secret = 'JGIImXrX99T1BTHeAe1LOaDu2aNNfix8l8rjv2fA4Sdwu'
