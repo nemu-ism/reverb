f5e8eb292cbf1e6fbf515303ce3ba006f6393cc3
nemu-ism <vanilla_chip_s@yahoo.co.jp>
2018-01-14 00:18:20 +0900

    inputからつぶやき

--- tweet_writer.py
+++ tweet_writer.py
@@ -1,7 +1,10 @@
 import reverb
+import twitter
 
 api = reverb.api
 
-#tweet_content = input("Your tweet is:").strip()
-
-api.update_status(status='hai')
+tweet_content = input("Your tweet is:").strip()
+try:
+    api.PostUpdates(tweet_content)
+except:
+    print("つぶやき内容の取得に失敗しました。")
